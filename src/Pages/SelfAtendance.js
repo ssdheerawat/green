732105fa85a97d99f05685ab8a997bf15488eedb
Sawai s <ssdheerawat@gmail.com>
2022-05-03 17:11:57 +0530

    changes

--- src/Pages/SelfAtendance.js
+++ src/Pages/SelfAtendance.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useRef  } from 'react';
 import { QrReader } from 'react-qr-reader';
+import { useTorchLight } from '@blackbox-vision/use-torch-light';
 
 const SelfAtendance = (props) => {
+    const streamRef = useRef(null);
   const [data, setData] = useState('No result');
+  const [on, toggle] = useTorchLight(streamRef.current);
+
 
   return (
     <>
+    <div  style={{ alignItems: 'center' }}>
+    <h5 class="card-header">Staff Attendance</h5>
+    <h6 class="card-subheader">Scan Stand QR Code</h6>
+    <div className='card1' style={{width:250, height:250, align:'center', margin:'auto'}}>
       <QrReader
        constraints={{
         facingMode: 'environment'
@@ -21,7 +29,12 @@ const SelfAtendance = (props) => {
         }}
         style={{ width: '100%' }}
       />
+      </div>
+
+      <button onClick={toggle}>{on ? 'Disable Torch' : 'Enable Torch'}</button>
+      </div>
       <p>{data}</p>
+
     </>
   );
 };
