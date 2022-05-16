32fee14104c3997090e2d9448e127a5dbe5cb273
Sawai s <ssdheerawat@gmail.com>
2022-05-16 09:00:09 +0530

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -93,7 +93,7 @@ const Header = (props) => {
             <img src={logo} alt="Logo" style={{    width: 38, height: 38, borderRadius:25, marginRight:15}} />
         
               <div className="navbar-nav align-items-center nav-title">
-                  GreenRide V4
+                  GreenRide V5
               </div>
          
               {isLoggedin   ? 
